7cd317185e452ac242b7e5aab687f12a4ede22b2
Jashan <jashanpreetsingh1049@gmail.com>
2025-10-23 16:17:43 +0000

    Add Compile time Polimorphism

--- classes.java
+++ classes.java
@@ -1,27 +1,26 @@
+// COMPILE-TIME POLIMORPHISM
+// Funtion Overloading
 class student{
     String name;
     int age;
-    public void info(){
-        System.out.println(this.name);
-        System.out.println(this.age);
+    public void info(String name, int age){
+        System.out.println("Name: " + name + "\nage: " + age);
     }
-    student(student s2){    
-       this.name =s2.name;
-       this.age =s2.age;    //copy constructor
+    public void info(String name){
+        System.out.println("Name: " + name);
     }
-    student(){
-        //default constructor
+    public void info(int age){
+        System.out.println("Age: " + age);
     }
-}
+    }
+    
+
 public class classes{
     public static void main(String[] args) {
         student s1= new student();
         s1.name="jashan";
         s1.age=19;
-        student s2= new student(s1);
-        s2.info();
-
-        
+        s1.info(s1.name, s1.age);
     }
         
 }
