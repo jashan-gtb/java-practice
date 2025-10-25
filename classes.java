8385a5c15cfbb62495bb21513112db7644c1b151
Jashan <jashanpreetsingh1049@gmail.com>
2025-10-25 14:38:03 +0000

    Add abstraction

--- classes.java
+++ classes.java
@@ -1,26 +1,24 @@
-// COMPILE-TIME POLIMORPHISM
-// Funtion Overloading
-class student{
-    String name;
-    int age;
-    public void info(String name, int age){
-        System.out.println("Name: " + name + "\nage: " + age);
-    }
-    public void info(String name){
-        System.out.println("Name: " + name);
-    }
-    public void info(int age){
-        System.out.println("Age: " + age);
-    }
-    }
-    
-
-public class classes{
-    public static void main(String[] args) {
-        student s1= new student();
-        s1.name="jashan";
-        s1.age=19;
-        s1.info(s1.name, s1.age);
-    }
-        
+// ABSTACTION
+abstract class animal{
+    abstract void walk();
 }
+class horse extends animal{
+    public void walk(){
+        System.out.println("walks on 4 legs");
+    }
+}
+class chicken extends animal{
+    public void walk(){
+        System.out.println("walks on 2 legs");
+    }
+}
+public class classes{
+    public static void main(String args[]){
+        horse h= new horse();
+        h.walk();
+        chicken c= new chicken();
+        c.walk();
+        animal a= new animal(); // This line will cause an error because you cannot instantiate an abstract class
+    }
+}
+
